94c7fb30c82a0119fac30418ef60a92707b5aab5
ksyusha <popovaksenia0000@gmail.com>
2023-09-18 16:04:55 +0700

    сделала задание 18 сентября

--- script.js
+++ script.js
@@ -4,4 +4,16 @@
         //используем встроенную функцию alert()                                                                                    
         alert('Изучать JavaScript - это офигенно, отвечаю!')
 
+
         //задача №1
+        console.log('JavaScript')
+        console.log('Flutter')
+        console.log('TypeScript')
+
+        console.log(2**4 + 3/2)
+        console.log("Ты просто супер! Правда!")
+
+        console.log(42/7)
+        console.log(2**4);
+        console.log(100 % 4);
+        console.log();
